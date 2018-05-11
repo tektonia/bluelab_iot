f7f5809ee2d7af8e6cfe3fb3a75fc3b13172fb7c
tektonia <vsilva@deetc.isel.ipl.pt>
2018-05-11 13:20:13 +0100

    Add files via upload
    Example

---
+++ BlueLabConnection.h
@@ -0,0 +1,49 @@
+#ifndef BLUELABCONNECTION_H
+#define BLUELABCONNECTION_H
+
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+
+class DataBaseConnection{
+  private:
+      HTTPClient http;  
+      int portHttp=80; 
+      String host_login;
+      String url_login;
+      String host_db;
+      String url_db;
+      String email;
+      String password;
+      int sessionId;
+      int numSeq;
+      const size_t JSON_BUFFER_SIZE = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 512;
+      String frame_head;
+      String frame_data;
+      String frame_debug;
+      int last_base;
+      int last_seq;
+      long long last_timeStamp;
+      String sendMessage(String host, int port, String url,String msg);
+  public:
+
+    DataBaseConnection(String l_host, String l_url, String d_host, String d_url);
+    bool login(String mail, String pass);
+    int getSessionId(int nBase);
+    String longLongToString(long long ll);
+    void newFrame(int base, int seq, long long timeStamp);
+    void addKeyValue(String key, String value);
+    void addKeyValue(String key, bool value); 
+    void addKeyValue(String key, char value); 
+    void addKeyValue(String key, int value);   
+    void addKeyValue(String key, long value); 
+    void addKeyValue(String key, long long value); 
+    void addKeyValue(String key, float value);   
+    void addKeyValue(String key, double value); 
+    void setDebug(bool deb);
+    String sendFrame();
+    String sendLastFrame();
+    int getSeqNum(int NumBase);
+};
+
+#endif /* BLUELABCONNECTION_H */
+
---
+++ BlueLab_IoT_demo.ino
@@ -0,0 +1,125 @@
+#include <ESP.h>
+#include <ESP8266WiFi.h>
+
+#include "BlueLabConnection.h"
+
+/**************************
+    Internet access
+change with your SSID and SSID_PASSWORD
+*******************/
+String ssid = "my_ssid";
+String ssid_password = "my_ssid_password";
+
+/**************************
+    BlueLab IoT system access
+change with your your credentials (you can use the demo credentials)
+*******************/
+String usr_email="a@a.a";
+String usr_password="a";
+
+// Stations should have unique ids (or ensure they have unique key strings)
+String moduleName = "Humi";
+int station_id = 1; 
+
+/**************************
+    BlueLab IoT links
+DO NOT CHANGE
+*******************/
+String login_host="www.tektonia.com.pt";
+String login_url="/tekton/entraaqui/login.php";
+String data_host="www.bluelab.pt";
+String data_url="/iot/call.php";  
+
+// maximum number of retries in case of error
+#define MAX_RETRIES 5
+#define DELAY_MILISECONDS 3000
+#define SAMPLE_COUNT 30
+
+DataBaseConnection *dbConn;
+  
+long long timeStamp=0;
+int seqNum;
+int count=0;
+  
+int adcPin=A0;
+int inPin=D0;
+
+
+void doReset(String err){
+  Serial.print("\nERROR - Resetting module: "+err+"\n");
+  EspClass esp;
+  esp.restart();
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("\nBlueLab IoT demo - VVS 2018\n\n"); 
+
+  Serial.println("\nStation id: "+String(station_id)+"\n");
+
+  dbConn = new DataBaseConnection(login_host, login_url, data_host, data_url);
+
+  Serial.print("Acquiring Internet access, through: "+ssid+" ...");
+  WiFi.begin(ssid.c_str(), ssid_password.c_str());
+  while (WiFi.status() != WL_CONNECTED) {  
+    if(count++>=MAX_RETRIES) doReset("No Internet Connection");
+    WiFi.begin(ssid.c_str(), ssid_password.c_str());
+    delay(3000);
+  }
+  Serial.println(" WiFi connected :):):)");
+
+  Serial.print("\nLogging in to BlueLab IoT system with "+usr_email+" ...");
+  count=0;
+  while(!dbConn->login(usr_email, usr_password))  {
+    Serial.println("Could not Login!");
+    if(count++>=MAX_RETRIES) doReset("No Access to the BlueLab Iot System. user:"+usr_email+": password:"+usr_password+":");
+    delay(3000);
+  }
+  Serial.println(" Logged in :):):)");
+  
+  seqNum=dbConn->getSeqNum(station_id);  //sequencia actual
+  seqNum++;
+
+  timeStamp=0;
+  dbConn->newFrame(station_id, seqNum, timeStamp);
+  dbConn->addKeyValue("reset",0);
+  String res=dbConn->sendFrame();
+  Serial.println("\nSendFrame: key=reset value=0 "+res+"\n");
+  if(!res.equals("RES_OK")) dbConn->login(usr_email, usr_password);
+  /**/
+
+  pinMode(inPin, INPUT);
+  count=SAMPLE_COUNT;
+}
+
+      
+void loop() {
+  delay(DELAY_MILISECONDS);
+  timeStamp+=DELAY_MILISECONDS;  // aproximatly
+  int din=digitalRead(inPin);
+  int adc=analogRead(adcPin);
+
+  Serial.print("Digital In: "+String(din)+" ADC: "+String(adc)+"\n");
+
+  if(count++>SAMPLE_COUNT){
+    count=0;
+
+    dbConn->newFrame(station_id, seqNum, timeStamp);
+    dbConn->addKeyValue("pin",din);
+    dbConn->addKeyValue("adc",adc);
+    
+    String res=dbConn->sendFrame();
+
+    Serial.println("SendFrame: key=pin value="+String(din)+"key=adc value="+String(adc)+" result: "+res+"\n");
+    if(res.equals("RES_OK")) seqNum++;
+    else{
+      if(res.equals("ERR_INVALID_SESSION_ID")){
+        dbConn->login(usr_email, usr_password);
+        dbConn->sendLastFrame();
+      }
+      seqNum=dbConn->getSeqNum(station_id)+1;
+    } 
+  }
+}
+
